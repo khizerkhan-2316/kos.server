077687975941fc86383815abe12394d8b4a04242
Khizer Khan <khizer759@gmail.com>
2024-04-24 15:37:52 +0200

    fixing serielservice

--- app.py
+++ app.py
@@ -1,11 +1,17 @@
 from flask import Flask, jsonify
 from flask_restx import Api
 from flask_cors import CORS
-
+from services.SerialService import SerialService
 
 from controllers.ActuatorController import ActuatorController
 
 app = Flask(__name__)
+
+
+@app.route("/")
+def hello_world():
+    return jsonify({"message": "Hello World"})
+
 
 cors = CORS(app)
 
@@ -13,7 +19,17 @@ app.config['CORS_HEADERS'] = 'Content-Type'
 
 actuator_controller = ActuatorController(app)
 
+serial_service = SerialService()
 
+serial_service.open_serial_connection()
+
+serial_service.send_command("T\n")
+
+print("reading")
+serial_service.read_status()
+print("done reading")
+
+serial_service.close_serial_connection()
 
 if __name__ == '__main__':
     app.run()
--- services/SerialService.py
+++ services/SerialService.py
@@ -1,8 +1,9 @@
 import serial
+import json
 
 
 class SerialService:
-    def __init__(self, port='/dev/ttyUSB0', baudrate=9600):
+    def __init__(self, port='COM4', baudrate=9600):
         self.port = port
         self.baudrate = baudrate
         self.serial = None
@@ -11,7 +12,21 @@ class SerialService:
         try:
             self.serial = serial.Serial(self.port, self.baudrate)
             print(f"Serial connection opened on port {self.port}")
+            print(f"To check which port is actually used: {self.serial.name}")
         except serial.SerialException as e:
             print(f"Error opening serial connection: {e}")
 
+    def send_command(self, data):
+        if self.serial:
+            self.serial.write(data.encode('utf-8'))
 
+    def read_status(self):
+        if self.serial:
+            x = self.serial.readline()
+            print(f"Recieved data:", x)
+
+
+    def close_serial_connection(self):
+        if self.serial and self.serial.is_open:
+            self.serial.close()
+            print("Serial connection closed")
